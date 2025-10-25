69c140713810da1eba5753b74d3048faba877f4b
codemaker-kim <tnals72441@daum.net>
2025-10-25 16:51:53 +0900

    refactor(input): 컨트롤러에 존재하던 입력 검증을 분리
    빈 입력 검증 로직과 List 파싱 과정을 거치던 검증을 Validator 클래스로 분리함

--- src/main/java/racingcar/controller/RacingCarGameController.java
+++ src/main/java/racingcar/controller/RacingCarGameController.java
@@ -1,10 +1,10 @@
 package racingcar.controller;
 
+import static java.util.stream.Collectors.toSet;
+
 import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 import racingcar.controller.validator.CarNameValidator;
 import racingcar.controller.validator.TryCountValidator;
 import racingcar.model.Car;
@@ -16,23 +16,15 @@ public class RacingCarGameController {
     private static final int BASE_POSITION = 0;
 
     public void play() {
-        String carNames = getCarNames();
+        String carNameInput = getCarNames();
         String tryCountInput = getTryCount();
 
-        CarNameValidator.validateCarNameIsEmpty(carNames);
-
-        List<String> carNameList = Arrays.stream(
-                carNames.split(COMMA)
-        ).toList();
-
-        CarNameValidator.validateCarNames(carNameList);
+        CarNameValidator.validateCarNames(carNameInput);
         TryCountValidator.validateTryCount(tryCountInput);
 
-        int tryCount = Integer.parseInt(tryCountInput);
+        Set<Car> carSet = collectToSet(carNameInput);
 
-        Set<Car> carSet = carNameList.stream()
-                .map(carName -> new Car(carName, BASE_POSITION))
-                .collect(Collectors.toSet());
+        int tryCount = Integer.parseInt(tryCountInput);
 
         for (int round = BASE_POSITION; round < tryCount; round++) {
             carSet.forEach(Car::accelerate);
@@ -51,6 +43,14 @@ public class RacingCarGameController {
         InputView.printCarNamePrompt();
 
         return Console.readLine();
+    }
+
+    private Set<Car> collectToSet(String carNameInput) {
+        return Arrays.stream(
+                        carNameInput.split(COMMA)
+                )
+                .map(carName -> new Car(carName, BASE_POSITION))
+                .collect(toSet());
     }
 }
 
--- src/main/java/racingcar/controller/validator/CarNameValidator.java
+++ src/main/java/racingcar/controller/validator/CarNameValidator.java
@@ -1,35 +1,45 @@
 package racingcar.controller.validator;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class CarNameValidator {
 
+    private static final String COMMA = ",";
     private static final String SPACE = " ";
     private static final int MAXIMUM_CARNAME_LENGTH = 5;
 
-    public static void validateCarNameIsEmpty(String carNameInput) {
+    public static void validateCarNames(String carNameInput) {
+        validateCarNameIsEmpty(carNameInput);
+
+        validateCarNamesHaveNoSpaces(carNameInput);
+
+        validateCarNamesAreUnique(carNameInput);
+
+        validateCarNamesTooLong(carNameInput);
+    }
+
+    private static void validateCarNameIsEmpty(String carNameInput) {
         if (carNameInput.isEmpty()) {
             throw new IllegalArgumentException("자동차 명이 비어있음");
         }
     }
 
-    public static void validateCarNames(List<String> carNameList) {
-        validateCarNamesHaveNoSpaces(carNameList);
-
-        validateCarNamesAreUnique(carNameList);
-
-        validateCarNamesTooLong(carNameList);
+    private static void validateCarNamesHaveNoSpaces(String carNameInput) {
+        Arrays.stream(
+                        carNameInput.split(COMMA)
+                )
+                .filter(
+                        carName -> carName.contains(SPACE)
+                )
+                .findFirst()
+                .ifPresent(carName -> {
+                    throw new IllegalArgumentException("자동차 명에 공백 포함");
+                });
     }
 
-    private static void validateCarNamesHaveNoSpaces(List<String> carNameList) {
-        for (String carName : carNameList) {
-            if (carName.contains(SPACE)) {
-                throw new IllegalArgumentException("자동차 명에 공백 포함");
-            }
-        }
-    }
-
-    private static void validateCarNamesAreUnique(List<String> carNameList) {
+    private static void validateCarNamesAreUnique(String carNameInput) {
+        List<String> carNameList = Arrays.asList(carNameInput.split(COMMA));
         long distinctCount = carNameList.stream()
                 .distinct()
                 .count();
@@ -39,11 +49,16 @@ public class CarNameValidator {
         }
     }
 
-    private static void validateCarNamesTooLong(List<String> carNameList) {
-        for (String carName : carNameList) {
-            if (carName.length() > MAXIMUM_CARNAME_LENGTH) {
-                throw new IllegalArgumentException("자동차 명은 5자 이하만 가능");
-            }
-        }
+    private static void validateCarNamesTooLong(String carNameInput) {
+        Arrays.stream(
+                        carNameInput.split(COMMA)
+                )
+                .filter(
+                        carName -> carName.length() > MAXIMUM_CARNAME_LENGTH
+                )
+                .findFirst()
+                .ifPresent(carName -> {
+                    throw new IllegalArgumentException("자동차 명은 5자 이하만 가능");
+                });
     }
 }
