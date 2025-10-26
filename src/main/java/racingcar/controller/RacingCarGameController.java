eed7e678f4399322576f637213869bce28d5268d
codemaker-kim <tnals72441@daum.net>
2025-10-26 22:52:09 +0900

    feat(output): 컨트롤러에 출력 기능 추가

--- src/main/java/racingcar/controller/RacingCarGameController.java
+++ src/main/java/racingcar/controller/RacingCarGameController.java
@@ -1,14 +1,16 @@
 package racingcar.controller;
 
+import static java.util.stream.Collectors.toCollection;
 import static java.util.stream.Collectors.toSet;
 
-import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
+import java.util.LinkedHashSet;
 import java.util.Set;
 import racingcar.controller.validator.CarNameValidator;
 import racingcar.controller.validator.TryCountValidator;
 import racingcar.model.Car;
 import racingcar.view.input.InputView;
+import racingcar.view.output.OutputView;
 
 public class RacingCarGameController {
 
@@ -16,8 +18,8 @@ public class RacingCarGameController {
     private static final int BASE_POSITION = 0;
 
     public void play() {
-        String carNameInput = getCarNames();
-        String tryCountInput = getTryCount();
+        String carNameInput = InputView.inputTryCount();
+        String tryCountInput = InputView.inputCarNames();
 
         CarNameValidator.validateCarNames(carNameInput);
         TryCountValidator.validateTryCount(tryCountInput);
@@ -26,23 +28,15 @@ public class RacingCarGameController {
 
         int tryCount = Integer.parseInt(tryCountInput);
 
+        OutputView.printResultPrompt();
+
         for (int round = BASE_POSITION; round < tryCount; round++) {
             carSet.forEach(Car::accelerate);
+            OutputView.printRoundResult(carSet);
         }
 
-        // 4. OutputView 기반 출력
-    }
-
-    private String getTryCount() {
-        InputView.printTryCountPrompt();
-
-        return Console.readLine();
-    }
-
-    private String getCarNames() {
-        InputView.printCarNamePrompt();
-
-        return Console.readLine();
+        Set<Car> winners = findWinners(carSet);
+        OutputView.printWinners(winners);
     }
 
     private Set<Car> collectToSet(String carNameInput) {
@@ -50,7 +44,18 @@ public class RacingCarGameController {
                         carNameInput.split(COMMA)
                 )
                 .map(carName -> new Car(carName, BASE_POSITION))
-                .collect(toSet());
+                .collect(toCollection(LinkedHashSet::new));
+    }
+
+    private Set<Car> findWinners(Set<Car> cars) {
+        int maxPosition = cars.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElse(BASE_POSITION);
+
+        return cars.stream()
+                .filter(car -> car.getPosition() == maxPosition)
+                .collect(toCollection(LinkedHashSet::new));
     }
 }
 
--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -15,4 +15,12 @@ public class Car {
         if (Randoms.pickNumberInRange(0, 9) >= 4)
             position++;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
 }
