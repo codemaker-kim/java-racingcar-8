7c65044a418042809b0067929e88b114bc0b4ed1
codemaker-kim <tnals72441@daum.net>
2025-10-27 15:17:05 +0900

    test(RacingCarWinnerStrategy): 올바르게 승자 리스트 목록을 만드는지 확인하는 테스트 코드 작성

---
+++ src/test/java/racingcar/strategy/RacingCarWinnerStrategyTest.java
@@ -0,0 +1,44 @@
+package racingcar.strategy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.model.Car;
+import racingcar.model.Cars;
+import racingcar.model.RacingCar;
+
+class RacingCarWinnerStrategyTest {
+
+    private WinnerStrategy winnerStrategy;
+
+    @BeforeEach
+    void setUp() {
+        winnerStrategy = new RacingCarWinnerStrategy();
+    }
+
+    @Test
+    @DisplayName("승자만이 포함된 리스트만을 올바르게 반환한다.")
+    void findWinnerTest() {
+        //given
+        final List<Car> carList = List.of(
+                new RacingCar("sumin", 3),
+                new RacingCar("java", 4),
+                new RacingCar("jigi", 5),
+                new RacingCar("gg", 5)
+        );
+        Cars cars = new Cars(carList);
+
+        //when
+        List<Car> winners = cars.findWinners(winnerStrategy);
+
+        //then
+        assertThat(winners)
+                .hasSize(2)
+                .extracting(Car::getName)
+                .containsExactlyInAnyOrder("jigi", "gg");
+    }
+}
