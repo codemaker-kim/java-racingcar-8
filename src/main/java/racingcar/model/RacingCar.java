7af2a5d7bb67b93c29a4dea5704de7dda81b4afe
codemaker-kim <tnals72441@daum.net>
2025-10-27 17:22:26 +0900

    refactor(RacingCar): accelerate 메서드 내 매직 넘버 상수화

--- src/main/java/racingcar/model/RacingCar.java
+++ src/main/java/racingcar/model/RacingCar.java
@@ -3,6 +3,9 @@ package racingcar.model;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class RacingCar extends Car {
+    private static final int RANDOM_MIN = 0;
+    private static final int RANDOM_MAX = 9;
+    private static final int ACCELERATION_THRESHOLD = 4;
 
     public RacingCar(String name, int position) {
         super(name, position);
@@ -10,7 +13,7 @@ public class RacingCar extends Car {
 
     @Override
     public void accelerate() {
-        if (Randoms.pickNumberInRange(0, 9) >= 4) {
+        if (Randoms.pickNumberInRange(RANDOM_MIN, RANDOM_MAX) >= ACCELERATION_THRESHOLD) {
             position++;
         }
     }
