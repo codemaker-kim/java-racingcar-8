9331f6c9667a0e9f893258eed82da971bbbaf54d
codemaker-kim <tnals72441@daum.net>
2025-10-27 16:41:28 +0900

    style(ErrorMessage): 에러 메시지 사용자 친화적으로 개선

--- src/main/java/racingcar/controller/validator/CarNameErrorMessage.java
+++ src/main/java/racingcar/controller/validator/CarNameErrorMessage.java
@@ -1,10 +1,10 @@
 package racingcar.controller.validator;
 
 public enum CarNameErrorMessage {
-    EMPTY_CAR_NAME("자동차 명이 비어있음"),
-    CONTAINS_SPACE("자동차 명에 공백 포함"),
-    DUPLICATE_NAME("중복 값 존재"),
-    TOO_LONG_NAME("자동차 명은 5자 이하만 가능");
+    EMPTY_CAR_NAME("자동차 이름 입력이 비어있습니다."),
+    CONTAINS_SPACE("자동차 이름에 공백이 포함되어 있습니다."),
+    DUPLICATE_NAME("자동차 이름이 중복됩니다."),
+    TOO_LONG_NAME("자동차 명은 5자 이하만 가능합니다.");
 
     private final String message;
 
--- src/main/java/racingcar/controller/validator/TryCountErrorMessage.java
+++ src/main/java/racingcar/controller/validator/TryCountErrorMessage.java
@@ -1,9 +1,9 @@
 package racingcar.controller.validator;
 
 public enum TryCountErrorMessage {
-    EMPTY_TRY_COUNT("시도 횟수가 비어있음"),
+    EMPTY_TRY_COUNT("시도 횟수 입력이 비어있습니다."),
     INVALID_NUMBER_FORMAT("시도 횟수는 숫자여야 합니다"),
-    BELOW_MINIMUM("최소 시도횟수는 1번");
+    BELOW_MINIMUM("최소 시도횟수는 1번입니다.");
 
     private final String message;
 
